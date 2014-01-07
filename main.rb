1735494749db41b33733f53b2915e8d221cc1fd4
lerak <karel1982@gmail.com>
2014-01-07 13:35:45 +0100

    changes

--- main.rb
+++ main.rb
@@ -3,24 +3,26 @@ require 'sinatra'
 
 set :sessions, true
 
-get '/' do 
-	if session[:player_name]
-		redirect '/game'
-	else
-		redirect '/new_player'
- end
+
+get '/' do
+  if session[:player_name]
+    redirect '/game'
+  else
+    redirect '/new_player'
+  end
 end
 
-get '/new_player' do 
-    erb :new_player
+get '/new_player' do
+  erb :new_player
 end
 
 post '/new_player' do
-	session[:player_name] = params[:player_name]
+  session[:player_name] = params[:player_name]
+  redirect '/game'
 end
 
-
-get '/game' do 
-	erb :game
+get '/game' do
+  
+  erb :game
 end
 
